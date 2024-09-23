eac23a9d751f9b30374a057f341b002461e43a04
David Tang <dev@blueur.com>
2024-09-23 11:33:49 +0200

    feat: add pwa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
 import Heading from "@theme/Heading";
+import Layout from "@theme/Layout";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
