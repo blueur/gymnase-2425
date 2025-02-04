bc0036ab0f6eb85c53c9b0013a03dddac07aa612
David Tang <dev@blueur.com>
2025-02-04 10:41:16 +0100

    feat: update complexity

--- src/pages/slides/encoding.tsx
+++ src/pages/slides/encoding.tsx
@@ -31,25 +31,48 @@ export default function Encoding(props: {
           ]}
         />
       </Section>
-      <Section level={3} title="Fichier texte">
-        <List
-          items={[
-            "Un fichier texte est composé d'une suite de **caractères**.",
-            "Chaque est représenté par un **code** en **octet**.",
-            [
-              "L'**encodage** définit la correspondance entre les caractères et les codes.",
-              [
-                "La table **ASCII** permet d'associer un code de 8 bits à un caractère.",
-                "Représentation en **hexadécimal** : `0x41` (0100 0001) pour le caractère `A`.",
-              ],
-            ],
-          ]}
+      <Section level={3} title="Fichier binaire">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/7/76/Wikipedia_favicon_hexdump.svg"
+          url="https://commons.wikimedia.org/wiki/File:Wikipedia_favicon_hexdump.svg"
         />
       </Section>
       <Section level={2} title="Table ASCII">
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/1/1b/ASCII-Table-wide.svg"
           url="https://commons.wikimedia.org/wiki/File:ASCII-Table-wide.svg"
+        />
+      </Section>
+      <Section level={2} title="Table ASCII">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/1/1b/ASCII-Table-wide.svg"
+          url="https://commons.wikimedia.org/wiki/File:ASCII-Table-wide.svg"
+          height="650px"
+        />
+        <List
+          fragment={[false, true]}
+          items={[
+            [
+              "0101 0110 0110 0101 0111 0010 0111 0100 = 0x56657274 = ?",
+              ["Vert"],
+            ],
+          ]}
+        />
+      </Section>
+      <Section level={3} title="Fichier texte">
+        <List
+          fragment
+          items={[
+            "Un fichier texte est composé d'une suite de **caractères**.",
+            "Chaque caractère est représenté par un **code** en **octet**.",
+            [
+              "Le **codage** définit la correspondance entre les caractères et les codes.",
+              [
+                "La table **ASCII** permet d'associer un code de 8 bits à un caractère.",
+                "Représentation en **hexadécimal** : `0x41` (0100 0001) pour le caractère `A`.",
+              ],
+            ],
+          ]}
         />
       </Section>
       <Section level={3} title="ASCII Art">
