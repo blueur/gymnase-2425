314057cac596cbf5edc700db579a2d7a5834adab
David Tang <dev@blueur.com>
2025-03-10 10:50:07 +0100

    feat: update spreadsheet

--- src/pages/slides/spreadsheet.tsx
+++ src/pages/slides/spreadsheet.tsx
@@ -1,4 +1,4 @@
-import { Section } from "@site/src/components/Deck";
+import { Image, List, Section } from "@site/src/components/Deck";
 import RevealDeck from "@site/src/components/RevealDeck";
 
 export default function Spreadsheet(props: {
@@ -12,7 +12,39 @@ export default function Spreadsheet(props: {
       title="Tableur"
       subTitle={props.chapter}
     >
-      <Section level={2} title="Tableur"></Section>
+      <Section level={2} title="Tableur">
+        <List
+          fragment
+          items={[
+            "Feuille de calcul (**tableau**)",
+            "Cellules",
+            [
+              "Logiciels",
+              ["Excel, Google Sheets, LibreOffice Calc, Numbers, &hellip;"],
+            ],
+          ]}
+        />
+      </Section>
+      <Section level={2} title="Nomeclature">
+        <Image
+          src="/2c-bure/interface_tableur.png"
+          url="http://prof.math.free.fr/afficher.php?id=73"
+        />
+      </Section>
+      <Section level={2} title="Calculs">
+        <List
+          fragment
+          items={[
+            "=32 + 10",
+            "=A1 * B1",
+            "=$D$4 - C5",
+            "=SOMME(A1:A10)",
+            "=MOYENNE(A3:B8)",
+            "=MIN(B4:B10)",
+            "=NB(C5:C9)",
+          ]}
+        />
+      </Section>
     </RevealDeck>
   );
 }
